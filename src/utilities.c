cd5468e60fd042ca1129eea2653da033b395c0cc
araml <lraffo@dc.uba.ar>
2019-03-15 23:39:43 -0300

    Change the split function so it supports different bayer patterns

--- src/utilities.c
+++ src/utilities.c
@@ -1,1 +1,6 @@
 #include <utilities.h>
+
+rgb split_raw12(uchar *raw12, size_t width, size_t height, cfa_pattern pattern) {
+    rgb res = {};
+    return res;
+}
--- src/utilities.h
+++ src/utilities.h
@@ -5,15 +5,24 @@
 extern "C" {
 #endif
 
+#include <stddef.h>
+
 typedef unsigned char uchar;
 
-struct rgb {
+typedef struct rgb {
     uchar *r;
     uchar *g;
     uchar *b;
-};
+} rgb;
 
-rgb split_raw12(const char *path_to_file);
+typedef enum cfa_pattern {
+    RGGB,  // raw12 pattern
+    BGGR,  // gonna implement the first two only
+    RGRB,
+    RGBG,
+} cfa_pattern;
+
+rgb split_raw12(uchar *raw12, size_t width, size_t height, cfa_pattern pattern);
 
 
 
